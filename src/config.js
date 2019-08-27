e464524f7eea213c035bfc57376669a9536af731
Traworler <editor@partumgeco.com>
2019-08-27 10:39:44 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+    appId: "63250bc3005392", //Enter your App ID
+    apiKey: "72152d653827ae9483f2c458900545e9264b8757", //Enter your API KEY
+    GUID: "vivek2227gupta", // Enter your group UID
+  };
